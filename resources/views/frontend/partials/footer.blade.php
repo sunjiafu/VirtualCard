ed78669baf10320e813e4d5b1497ac13ee45aeee
unknown <84288682@qq.com>
2024-09-08 22:10:42 +0800

    google any

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -46,4 +46,14 @@
             </div>
         </div>
     </div>
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5QT8DEH80J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5QT8DEH80J');
+</script>
 </footer>
