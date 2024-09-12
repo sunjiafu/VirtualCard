8cc4dfa03947ba44e10cca2d0e2672de45ec3b65
unknown <84288682@qq.com>
2024-09-12 19:55:18 +0800

    google gtag

--- resources/views/user/partials/side-nav.blade.php
+++ resources/views/user/partials/side-nav.blade.php
@@ -131,6 +131,14 @@
             openAlertModal(actionRoute,target,message,logout,"POST");
         });
     </script>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5QT8DEH80J"></script>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5QT8DEH80J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5QT8DEH80J');
+</script>
 
 @endpush
