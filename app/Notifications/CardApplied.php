60cfb221bb51f0d32ffd93c036446e08c6c5fdf4
unknown <84288682@qq.com>
2024-11-14 15:53:06 +0800

    create card

--- app/Notifications/CardApplied.php
+++ app/Notifications/CardApplied.php
@@ -28,14 +28,20 @@ class CardApplied extends Notification
     {
         $message = "<b>✅ 新卡片申请通知</b>\n\n";
         
+        // 对内容进行 HTML 实体编码
+        $userInfo = htmlspecialchars("{$this->user->fullname}\n{$this->user->email}", ENT_QUOTES, 'UTF-8');
+        $maskedCard = htmlspecialchars($this->card->masked_card, ENT_QUOTES, 'UTF-8');
+        $applyTime = htmlspecialchars(now()->format('Y-m-d H:i:s'), ENT_QUOTES, 'UTF-8');
+    
         // 使用 <code> 标签创建可复制的蓝色背景文本块
-        $message .= "用户信息：\n<code>{$this->user->fullname}\n{$this->user->email}</code>\n\n";
-        $message .= "卡号：\n<code>{$this->card->masked_card}</code>\n\n";
-        $message .= "申请时间：\n<code>" . now()->format('Y-m-d H:i:s') . "</code>";
-
+        $message .= "用户信息：\n<code>{$userInfo}</code>\n\n";
+        $message .= "卡号：\n<code>{$maskedCard}</code>\n\n";
+        $message .= "申请时间：\n<code>{$applyTime}</code>";
+    
         return TelegramMessage::create()
             ->to(env('TELEGRAM_ADMIN_CHAT_ID'))
             ->content($message)
             ->options(['parse_mode' => 'HTML']); // 使用 options 方法设置 parse_mode
     }
+    
 }
